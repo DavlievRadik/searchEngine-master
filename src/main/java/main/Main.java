820846d828ae02a0f7217c92a463bc89d0655eef
Radik Davliev <superglad8763@gmail.com>
2023-03-03 12:50:45 +0300

    Diplom project

--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -2,8 +2,6 @@ package main;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 @SpringBootApplication
 public class Main {
